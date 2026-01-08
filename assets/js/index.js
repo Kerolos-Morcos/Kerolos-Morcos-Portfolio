bded4a9e6d2b97bb518e6ba0b42a13693248fa09
Kerolos-Morcos <122631134+Kerolos-Morcos@users.noreply.github.com>
2026-01-09 01:16:55 +0200

    Scroll To Top Feature

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,23 @@
-// ^ Write your JavaScript code here
+// Scroll To Top
+const scrollToTopBtn = document.getElementById("scroll-to-top");
 
+scrollToTopFeature();
+function scrollToTopFeature() {
+  // On Scroll Event
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+      scrollToTopBtn.classList.remove("opacity-0", "invisible");
+      scrollToTopBtn.classList.add("opacity-100", "visible");
+    } else {
+      scrollToTopBtn.classList.add("opacity-0", "invisible");
+      scrollToTopBtn.classList.remove("opacity-100", "visible");
+    }
+  });
+  // On Click Event
+  scrollToTopBtn.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+}
