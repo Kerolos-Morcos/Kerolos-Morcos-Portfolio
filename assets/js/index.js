ee14070944ae5d93fc5d5269b13b16399c9591ba
Kerolos-Morcos <122631134+Kerolos-Morcos@users.noreply.github.com>
2026-01-09 02:16:12 +0200

    Active Links Feature

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,11 @@
 // Scroll To Top
 const scrollToTopBtn = document.getElementById("scroll-to-top");
+// Active Links
+const sections = document.querySelectorAll("section");
+const links = document.querySelectorAll(".nav-links a");
+const navHeight = document.getElementById("header").offsetHeight;
 
+// Scroll To Top Feature
 scrollToTopFeature();
 function scrollToTopFeature() {
   // On Scroll Event
@@ -19,5 +24,23 @@ function scrollToTopFeature() {
       top: 0,
       behavior: "smooth",
     });
+  });
+}
+
+// Active Links Feature
+activeLinks();
+function activeLinks() {
+  window.addEventListener("scroll", () => {
+    let current = "";
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop - navHeight - 10;
+      if (window.scrollY >= sectionTop) {
+        current = section.id;
+      }
+    });
+    links.forEach((link) => link.classList.remove("active"));
+    document
+      .querySelector(`.nav-links a[href="#${current}"]`)
+      .classList.add("active");
   });
 }
